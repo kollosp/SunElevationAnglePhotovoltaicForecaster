b7bd395e543b36a6f8a44aa3a8f93ba67db9d82d
kollosp <pparczyk@wp.pl>
2024-08-14 08:00:02 +0200

    changed howto script

--- __main__.py
+++ __main__.py
@@ -2,8 +2,10 @@ from datasets import utils
 from SEAPF.Model import Model
 from matplotlib import pyplot as plt
 from SEAPF.Plotter import Plotter
+from sklearn.metrics import mean_absolute_error
 
-
+SAMPLES_PER_HOUR=12
+SAMPLES_PER_DAY=12
 
 """
     Show dataset if the script was run directly instead of being loaded as package
@@ -15,14 +17,20 @@ if __name__ == "__main__":
     # Use all columns to create X
     # X = utils.timeseries_to_dataset([data[i] for i in data.columns], window_size=1)
 
-    production = data["Production"].to_numpy()
-    print(production)
+    y = data["Production"].to_numpy()
+    X =  ts.reshape(1,-1).T #make 2D
 
-    model = Model(latitude_degrees=utils.LATITUDE_DEGREES, longitude_degrees=utils.LONGITUDE_DEGREES, x_bins=30,
-                 y_bins=60, bandwidth=0.4)
-    model.fit(ts=ts, data=production,  zeros_filter_modifier = -0.4, density_filter_modifier = -0.5)
 
-    pred = model.predict(ts)
-    plotter = Plotter(ts, [production, pred], debug=True)
+    model = Model(window_size=SAMPLES_PER_HOUR*3, latitude_degrees=utils.LATITUDE_DEGREES, longitude_degrees=utils.LONGITUDE_DEGREES, x_bins=30,
+                 y_bins=60, bandwidth=0.4, interpolation=True)
+    # in sample training - only for test
+    model.fit(X=X, y=y,  zeros_filter_modifier = -0.2, density_filter_modifier = -0.4)
+    pred = model.predict(X)
+
+    model.plot()  # for displaying model parameters
+    plotter = Plotter(ts, [y, pred], debug=True)
+
+    print(f"In-sample MAE: {mean_absolute_error(y, pred)}")
+
     plotter.show()
     plt.show()
